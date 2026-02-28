b8448e2cbed01a42744baed16a1ae009c78a1b6f
minyeophan <gksalsduq2@gmail.com>
2026-02-28 17:45:51 +0900

    feat: React + TypeScript + Vite 프론트엔드 초기 세팅

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import HelloBlueprint from '../components/HelloBlueprint';
+
+function HomePage() {
+  return (
+    <div>
+      <HelloBlueprint name="Blueprint" />
+    </div>
+  );
+}
+
+export default HomePage;
